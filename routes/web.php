fc65a3223dd440d536dea8f09dbf303657b5b513
Azkawdk <rilloazkawdk@gmail.com>
2026-09-02 11:31:58 +0700

    [P-2] setup routing resource semua tabel

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route Group Admin
+Route::prefix('admin')->group(function () {
+    Route::get('/info', function () {
+        return 'Ini adalah halaman informasi Admin';
+    });
+});
